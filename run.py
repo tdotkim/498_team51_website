eca3b2547418b0975456d8e26d3f8bf9fc4b9475
tdotkim <13062439+tdotkim@users.noreply.github.com>
2024-02-21 00:08:22 -0500

    chang ack to 8080

--- run.py
+++ run.py
@@ -47,4 +47,4 @@ if DEBUG:
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 else:
-    app.run(host="0.0.0.0", use_reloader=False)
+    app.run(host="0.0.0.0", port=8080, use_reloader=False)
