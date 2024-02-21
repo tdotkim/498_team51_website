e13fab9ff7c3a54505ec73e2fa4c5732017908ee
tdotkim <13062439+tdotkim@users.noreply.github.com>
2024-02-20 20:00:06 -0500

    fixed chatbot

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -13,6 +13,36 @@ from google.cloud import storage
 import pandas as pd
 import os
 
+
+def chatbot_request(query):
+    CREDS = "C:/Users/tkkim/gcp_keys/capstone-team51-366963bafc54.json"
+    storage_client = storage.Client.from_service_account_json(json_credentials_path=CREDS,project='capstone-team51')
+    bq_client = bigquery.Client.from_service_account_json(json_credentials_path=CREDS,project='capstone-team51')
+
+    bucket = storage_client.get_bucket('capstone-team51-data')
+    os.environ["OPENAI_API_KEY"] = "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"
+    df = pd.read_excel(bucket.blob('chatbot/ChicagoCrimeFeb24.xlsx').download_as_string())
+
+
+    ## For excel spreadsheet
+    text_data = df['Text'].tolist()
+
+    # # Remove new-line characters
+    for i in range(len(text_data)):
+        text_data[i] = text_data[i].replace("\n", " ")
+
+    # # convert Excel spreadsheet to documents
+    documents = [Document(text=t) for t in text_data]
+
+    index = GPTVectorStoreIndex.from_documents(documents)
+
+    query_engine = index.as_query_engine()
+    result = query_engine.query(query)
+    return result
+
+    #client = bigquery.Client(project='capstone-team51')
+
+
 @blueprint.route('/index')
 #@login_required
 def index():
@@ -56,37 +86,15 @@ def get_segment(request):
     except:
         return None
     
-def chatbot_request(query):
-    CREDS = "C:/Users/tkkim/gcp_keys/capstone-team51-366963bafc54.json"
-    storage_client = storage.Client.from_service_account_json(json_credentials_path=CREDS,project='capstone-team51')
-    bq_client = bigquery.Client.from_service_account_json(json_credentials_path=CREDS,project='capstone-team51')
-
-    bucket = storage_client.get_bucket('capstone-team51-data')
-    os.environ["OPENAI_API_KEY"] = "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"
-    df = pd.read_excel(bucket.blob('chatbot/ChicagoCrimeFeb24.xlsx').download_as_string())
 
 
-    ## For excel spreadsheet
-    text_data = df['Text'].tolist()
-
-    # # Remove new-line characters
-    for i in range(len(text_data)):
-        text_data[i] = text_data[i].replace("\n", " ")
-
-    # # convert Excel spreadsheet to documents
-    documents = [Document(text=t) for t in text_data]
-
-    index = GPTVectorStoreIndex.from_documents(documents)
-
-    query_engine = index.as_query_engine()
-    result = query_engine.query(query)
-    return result
-
-    #client = bigquery.Client(project='capstone-team51')
-
-@blueprint.route("/get")
+@blueprint.route("/result", methods = ['POST', 'GET'])
 def get_bot_response():    
-    userText = request.form.get('query')  
-    result = chatbot_request(userText)  
-    #return str(bot.get_response(userText)) 
-    return render_template("home/chatbot.html", segment="chatpot", result=result)
+    if request.method == 'POST': 
+        form_data = request.form
+        print(form_data)
+        userText = form_data['ting']
+        #userText = request.form.get('query')  
+        result = chatbot_request(userText)  
+        #return str(bot.get_response(userText)) 
+        return render_template("home/chatbot.html", segment="chatbot", result=result)
