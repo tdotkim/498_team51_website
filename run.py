a3dc5b0c7bc4fec2f826a76543e0efa80b4d37b5
tdotkim <13062439+tdotkim@users.noreply.github.com>
2024-02-21 01:20:07 -0500

    Removed migrate

--- run.py
+++ run.py
@@ -9,7 +9,7 @@ from   flask_minify  import Minify
 from   sys import exit
 
 from apps.config import config_dict
-from apps import create_app, db
+from apps import create_app
 
 
 from google.cloud import bigquery
@@ -33,7 +33,6 @@ except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
 app = create_app(app_config)
-Migrate(app, db)
 
 if not DEBUG:
     Minify(app=app, html=True, js=False, cssless=False)
