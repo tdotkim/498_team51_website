3616f8c0425b44e4a8fa408186d129bd9688d4ce
tdotkim <13062439+tdotkim@users.noreply.github.com>
2024-02-21 01:26:33 -0500

    more db refs removed

--- apps/__init__.py
+++ apps/__init__.py
@@ -13,7 +13,7 @@ import os
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home'):
+    for module_name in ('home'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
--- apps/home/routes.py
+++ apps/home/routes.py
@@ -5,7 +5,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 from apps.home import blueprint
 from flask import render_template, request
-from flask_login import login_required
 from jinja2 import TemplateNotFound
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
 from google.cloud import bigquery
@@ -46,13 +45,13 @@ def chatbot_request(query):
 
 @blueprint.route('/')
 @blueprint.route('/index')
-#@login_required
+
 def index():
 
     return render_template('home/index.html', segment='index')
 
 @blueprint.route('/<template>')
-#@login_required
+
 def route_template(template):
 
     try:
